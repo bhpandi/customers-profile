d0443c4e96fe5b518febf67a92cc4f90a8926b57
bhpandi <bhpandi@paypal.com>
2018-11-16 18:15:26 +0530

    Updates

---
+++ precache-manifest.bc7a4640b4c8a5b6052a28fda7c32df5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2e8d376e5bd5da38716",
+    "url": "/customers-profile/static/js/runtime~main.c2e8d376.js"
+  },
+  {
+    "revision": "13c5e033a6e9f1ddc333",
+    "url": "/customers-profile/static/js/main.13c5e033.chunk.js"
+  },
+  {
+    "revision": "d9f2c48e8e9770c90fc0",
+    "url": "/customers-profile/static/js/1.d9f2c48e.chunk.js"
+  },
+  {
+    "revision": "13c5e033a6e9f1ddc333",
+    "url": "/customers-profile/static/css/main.fd69decb.chunk.css"
+  },
+  {
+    "revision": "d9f2c48e8e9770c90fc0",
+    "url": "/customers-profile/static/css/1.4688a0eb.chunk.css"
+  },
+  {
+    "revision": "5b61180c44a0d8475d0787edc5e3a459",
+    "url": "/customers-profile/index.html"
+  }
+];
